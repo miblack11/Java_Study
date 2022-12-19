27c3b591cf45f0d342597910b870c31f83cb54e2
miblack11 <120943422+miblack11@users.noreply.github.com>
2022-12-19 18:01:46 +0800

    first add diles
    变量

---
+++ Var01.java
@@ -0,0 +1,11 @@
+public class Var01{
+	public static void main(String[] args){
+		//方法1，声明一个变量
+		int a;
+		a = 100;
+		//方法2，一步到位
+		int b = 100;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
